531b7fb41c20c1f457382c59816f7e81136867f0
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-12-10 09:54:56 +1300

    Add grid map reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { SET_CURRENT_COLOR } from '../actions';
+import { Map } from 'immutable';
 
 const colorInitState = {
   currentColor: '#00FF00',
@@ -16,5 +17,12 @@ const toolInitState = {
   currentTool: 'DRAW',
 };
 export function tool(state = toolInitState, action) {
+  return state;
+}
+
+const gridInitState = {
+  grid: Map()
+}
+export function grid(state = gridInitState, action) {
   return state;
 }
